7e2b213af76d9fa196bb93b91c761b615af6bf1f
Duc Hoang Manh <jandyz.dev@gmail.com>
2024-11-21 22:33:37 +0700

    adjusted logo application

--- app/build/generated/source/buildConfig/androidTest/debug/com/qguxxi/synthvoice/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/qguxxi/synthvoice/test/BuildConfig.java
@@ -9,4 +9,6 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
   public static final String API_KEY = "530061469984-oohp5fqmi6em05pos0pj42glook1s7k0.apps.googleusercontent.com";
+  // Field from default config.
+  public static final String OPEN_AI_API_KEY = "530061469984-oohp5fqmi6em05pos0pj42glook1s7k0.apps.googleusercontent.com";
 }
